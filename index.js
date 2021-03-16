cb9100cf17f77755c46ed392934c8de729b7dead
Xiaochen Xu <xiaochex@gmail.com>
2021-03-15 22:38:45 -0400

    commit tree 7d5c0c84fc528579e414281c2f9b5249747b35d7

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let greeting
+  if(distance <= 400){
+    greeting = 'This one is on me!'
+  }else if(distance > 2000 && distance < 2500) {
+    greeting = 'I will gladly take your thirty bucks.'
+  }else if(distance > 2500){
+    greeting = 'No can do.'
+  }
+  return greeting
+}
+scuberGreetingForFeet(2501)
+
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return(city==="NYC" ? "Ok, sounds good." : "No go.");
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  switch (tip){
+      case 'generous':
+           return('Thank you so much.');
+       
+         
+      case 'not as generous':
+            return('Thank you.');
+           
+
+      default:
+           return('Bye.'); 
+           
+          
+  }
 }
